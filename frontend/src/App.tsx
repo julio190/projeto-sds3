21dbd389162a07dfbd665b15399886baf227fd2e
julio190 <juliocezar_sousa@hotmail.com>
2021-05-13 10:50:05 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div >
+    <>
+      <NavBar />
 
-      <h1 className="text-primary">Ola mundo!</h1>
+      <div className="container">
 
-    </div>
+        <h1 className="text-primary">Ola mundo!</h1>
+        <DataTable />
+
+
+      </div>
+      <Footer/>
+    </>
   );
 }
 
